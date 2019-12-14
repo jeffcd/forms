eb40ce377508e662f96ea52962cf360e227698eb
Jeff Decker <jeff@jeffcd.com>
2019-12-13 22:49:57 -0800

    update for latest Mess Hall and remove Back as no backend is needed for this

--- messhall.config.js
+++ messhall.config.js
@@ -1,3 +1,26 @@
-module.exports = {
-  adapter: 'none'
+export default {
+  router: {
+    basename: '/'
+  },
+  routes: [
+    {
+      path: '/',
+      exact: true,
+      page: 'Home'
+    },
+    {
+      path: '/list',
+      exact: true,
+      page: 'List'
+    }
+  ],
+  transports: {
+    default: {
+      type: 'http',
+      opts: {
+        base: '/api/'
+      },
+      endpoints: {}
+    }
+  }
 }
